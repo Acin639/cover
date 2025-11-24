9ffa0fe37c7b2cb1642af3d656d6ab5310f92e4e
Acin639 <titusnjeru48@gmail.com>
2025-11-24 21:49:30 +0300

    Hi

--- src/components/searcher/Results.jsx
+++ src/components/searcher/Results.jsx
@@ -1,20 +1,22 @@
-
 import React from 'react';
 import search_icon from '../../assets/search_icon.svg';
-import {useNavigate,useParams} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
+export default function Results({ movies }) {
+  const navigate = useNavigate();
 
-export default function Results({movies}) {
-  const navigate = useNavigate()
   return (
-    <div className="results-box" onClick={navigate(`/stream/${movie.id}`}>
+    <div className="results-box">
       {movies.map(movie => (
-        <div key={movie.id} className="result-item">
+        <div
+          key={movie.id}
+          className="result-item"
+          onClick={() => navigate(`/stream/${movie.id}`)}
+        >
           <img src={search_icon} className="result-icon" alt="" />
-          {movie.title + (movie.release_date ? ` (${movie.release_date.split('-')[0]})` : '')} 
+          {movie.title + (movie.release_date ? ` (${movie.release_date.split('-')[0]})` : '')}
         </div>
       ))}
     </div>
   );
 }
-
